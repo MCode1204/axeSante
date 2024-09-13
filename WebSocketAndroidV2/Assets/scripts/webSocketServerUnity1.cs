a1983cfed1dad0a06b150a0134fab282807f409a
NoR47000 <94907401+NoR47000@users.noreply.github.com>
2024-09-13 10:39:53 +0200

    Assets

---
+++ WebSocketAndroidV2/Assets/scripts/webSocketServerUnity1.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using WebSocketSharp.Server;
+using WebSocketSharp;
+using UnityEngine.SceneManagement;
+
+public class WebSocketServerUnity : MonoBehaviour
+{
+    private WebSocketServer wss;
+
+    // Démarre le serveur WebSocket
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);  // Rend cet objet persistant entre les scènes
+
+        // Démarre le serveur WebSocket sur localhost, port 8080
+        wss = new WebSocketServer("ws://172.21.160.1:8080");
+        wss.AddWebSocketService<SceneHandler>("/SceneChange");
+        wss.Start();
+        Debug.Log("Serveur WebSocket démarré sur ws://172.21.160.1:8080/SceneChange");
+    }
+
+    // Ferme le serveur WebSocket lorsque l'application est fermée
+    void OnDestroy()
+    {
+        if (wss != null)
+        {
+            wss.Stop();
+            wss = null;
+            Debug.Log("Serveur WebSocket arrêté.");
+        }
+    }
+
+    // Classe pour gérer les messages WebSocket
+    public class SceneHandler : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            // Message reçu contenant le nom de la scène
+            string sceneName = e.Data;
+
+            // Charger la scène dans Unity
+            if (!string.IsNullOrEmpty(sceneName))
+            {
+                Debug.Log("Chargement de la scène :" + sceneName+"|");
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
+}
